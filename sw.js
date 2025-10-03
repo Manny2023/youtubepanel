1b3a57955204fea9e2cb4ad025d52ffa9d4f84a0
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2025-10-03 14:20:44 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,34 +3,26 @@ const urlsToCache = [
   '.',
   './index.html',
   './manifest.json',
-  // Agrega recursos estáticos aquí: css, imágenes, iconos, scripts
+  // añadir aquí recursos extras si los hubiere
 ];
 
-// Instalación y cacheo de recursos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activación: limpieza de caches antiguos si es necesario
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if(key !== CACHE_NAME){
-          return caches.delete(key);
-        }
-      })
+      keys.map(key => (key !== CACHE_NAME) ? caches.delete(key) : null)
     ))
   );
 });
 
-// Intercepción de peticiones para dar recursos cacheados
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
   );
 });
